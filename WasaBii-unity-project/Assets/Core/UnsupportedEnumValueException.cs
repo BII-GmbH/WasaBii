dda7226810cbae3114a8b5f2ca1347ef2c7fc713
Gollorum <david-schantz@gmx.de>
2022-01-12 15:12:25 +0100

    Revert "Merge remote-tracking branch 'origin/dProB-import' into core-lib-dump"
    This reverts commit d1b46b74d82330b6429f88e7a9913b092c25af0c, reversing
    changes made to 1491263b5e6ec586c17450d337e115898b432808.

--- WasaBii-unity-project/Assets/Core/UnsupportedEnumValueException.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BII.WasaBii.Core {
-    public class UnsupportedEnumValueException : Exception {
-        public UnsupportedEnumValueException(Enum value, string context)
-            : base($"The {value.GetType().Name} value {value} is not supported in {context}") { }
-    }
-}
--- WasaBii-unity-project/Assets/Unity/Extensions/ListExtensions.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-namespace BII.Utilities.Unity {
-    public static class ListExtensions {
-        public static void AddRange<T>(this List<T> source, params T[] items) => source.AddRange(items);
-    }
-}

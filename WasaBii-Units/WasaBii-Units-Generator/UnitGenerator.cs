a80bc812bce35c70e3ebde0ec8988a6edea055db
Cameron Reuschel <reuschel.cameron@live.de>
2022-04-21 04:31:12 +0200

    Derived units basics

--- WasaBii-Units/WasaBii-Units-Generator/UnitGenerator.cs
+++ WasaBii-Units/WasaBii-Units-Generator/UnitGenerator.cs
@@ -13,14 +13,15 @@ record UnitDef(string Name, string Short, double Factor);
 
 record BaseUnit(string TypeName, SiUnitDef SiUnit, UnitDef[] AdditionalUnits, bool GenerateExtensions = false);
 
-enum DerivedType { Div, Mul }
+record DerivedUnit(string TypeName, 
+    string PrimaryUnit, 
+    string SecondaryUnit, 
+    SiUnitDef SiUnit,
+    UnitDef[] AdditionalUnits, 
+    bool GenerateExtensions = false
+);
 
-record DerivedDef(DerivedType Type, string Primary, string Secondary);
-
-record DerivedUnit(string TypeName, DerivedDef Derived, UnitDef[] AdditionalUnits, 
-    bool GenerateExtensions = false, bool GenerateDerivedUnits = false);
-
-record UnitDefinitions(string Namespace, BaseUnit[] BaseUnits, DerivedUnit[] DerivedUnits);
+record UnitDefinitions(string Namespace, BaseUnit[] BaseUnits, DerivedUnit[] MulUnits, DerivedUnit[] DivUnits);
 
 #endregion
 
@@ -83,8 +84,6 @@ public class UnitGenerator : ISourceGenerator {
 namespace System.Runtime.CompilerServices {
     public static class IsExternalInit {}
 }", Encoding.UTF8));
-
-        
     }
 
     private static SourceText GenerateSourceFor(
@@ -97,7 +96,10 @@ using BII.WasaBii.Units;
 {string.Join("\n\n", unitDef.BaseUnits.Select(GenerateBaseUnit))}
 
 
-{string.Join("\n\n", unitDef.DerivedUnits.Select(GenerateDerivedUnit))}
+{string.Join("\n\n", unitDef.MulUnits.Select(m => GenerateDerivedUnit(m, isMul: true)))}
+
+
+{string.Join("\n\n", unitDef.DivUnits.Select(d => GenerateDerivedUnit(d, isMul: false)))}
 ";
         
         return SourceText.From(
@@ -105,6 +107,8 @@ using BII.WasaBii.Units;
             Encoding.UTF8
         );
     }
+    
+#region Base Units
 
     private static string GenerateBaseUnit(BaseUnit unit) {
         var name = unit.TypeName;
@@ -182,10 +186,10 @@ public readonly struct {name} : IUnitValue<{name}, {name}.Unit> {{
 
 public static class {name}ToDoubleExtensions {{
     public static double As{unit.SiUnit.Name}(this IUnitValue<{name}.Unit> value) => value.SiValue;
-{string.Join("\n", unit.AdditionalUnits.Select(au => 
+{string.Join("\n", unit.AdditionalUnits.Select(au =>
     $@"    public static double As{au.Name}(this IUnitValue<{name}.Unit> value) => value.SiValue / {au.Factor};"))}  
 }}
-"
+";
     }
 
     private static string GenerateConstructionExtensions(BaseUnit unit) {
@@ -201,11 +205,108 @@ public static class {name}ConstructionExtensions {{
 ";
     }
     
+#endregion
 
-    private static string GenerateDerivedUnit(DerivedUnit unit) {
-        return "";
+#region Derived Units
+
+    private static string GenerateDerivedUnit(DerivedUnit unit, bool isMul) {
+        var name = unit.TypeName;
+        var typeStr = isMul ? "Mul" : "Div";
+        return $@"#region {name}
+
+public readonly struct {name} : IUnitValue<{name}, {name}.Unit> {{
+    public double SiValue {{ init; get; }}
+    public Type UnitType => typeof(Unit);
+    
+    public {name}(double value, Unit unit) => SiValue = value * unit.SiFactor;
+
+    public static {name} Zero => new(0, Unit.SiUnit);
+
+    [UnitMetadata(typeof(Description))]
+    public abstract class Unit : IUnit.{typeStr}<{unit.PrimaryUnit}.Unit, {unit.SecondaryUnit}.Unit> {{
+        public abstract string LongName {{ get; }}
+        public abstract string ShortName {{ get; }}
+        public abstract double SiFactor {{ get; }}
+
+        private Unit() {{}}
+
+        public static Unit SiUnit => {unit.SiUnit.Name}.Instance;
+
+        public sealed class Description : IUnitDescription<Unit> {{
+            public Unit SiUnit => Unit.SiUnit;
+        }}
+
+        public sealed class {unit.SiUnit.Name} : Unit {{
+            public override string LongName => ""{unit.SiUnit.Name}"";
+            public override string ShortName => ""{unit.SiUnit.Short}"";
+            public override double SiFactor => 1d;
+            
+            private {unit.SiUnit.Name}() : base() {{}}
+
+            public static readonly {unit.SiUnit.Name} Instance = new();
+        }}
+
+{string.Join("\n\n", unit.AdditionalUnits.Select(au => $@"
+        public sealed class {au.Name} : Unit {{
+            public override string LongName => ""{au.Name}"";
+            public override string ShortName => ""{au.Short}"";
+            public override double SiFactor => {au.Factor};
+            
+            private {au.Name}() : base() {{}}
+
+            public static readonly {au.Name} Instance = new();
+        }}
+"))}
+
+    }}
+
+    public int CompareTo({name} other) => this.SiValue.CompareTo(other.SiValue);
+    public bool Equals({name} other) => this.SiValue.Equals(other.SiValue);
+
+    public static bool operator ==({name} left, {name} right) => Equals(left, right);
+    public static bool operator !=({name} left, {name} right) => !Equals(left, right);
+
+    public override bool Equals(object obj) => obj is {name} other && Equals(other);
+
+    // We include this type in case values of different units are hashed in the same collection
+    public override int GetHashCode() => HashCode.Combine(this.SiValue, typeof({name}));
+
+    {/* TODO: operators with this and other compatible units */ ""}
+    
+}}{GenerateDerivedToDoubleExtensions(unit)}{GenerateDerivedConstructionExtensions(unit)}
+
+#endregion
+";
+    }
+
+    private static string GenerateDerivedToDoubleExtensions(DerivedUnit unit) {
+        if (!unit.GenerateExtensions) return "";
+        var name = unit.TypeName;
+        return $@"
+
+    public static class {name}ToDoubleExtensions {{
+        public static double As{unit.SiUnit.Name}(this IUnitValue<{name}.Unit> value) => value.SiValue;
+    {string.Join("\n", unit.AdditionalUnits.Select(au =>
+        $@"    public static double As{au.Name}(this IUnitValue<{name}.Unit> value) => value.SiValue / {au.Factor};"))}  
+    }}
+    ";
+    }
+
+    private static string GenerateDerivedConstructionExtensions(DerivedUnit unit) {
+        if (!unit.GenerateExtensions) return "";
+        var name = unit.TypeName;
+        return $@"
+
+    public static class {name}ConstructionExtensions {{
+        public static {name} {unit.SiUnit.Name}(this double value) => new(value, {name}.Unit.{unit.SiUnit.Name}.Instance);
+    {string.Join("\n", unit.AdditionalUnits.Select(au => 
+        $@"    public static {name} {au.Name}(this double value) => new(value, {name}.Unit.{au.Name}.Instance);"))}
+    }}
+    ";
     }
     
+#endregion
+
     private static string InNamespace(string code, string nameSpace) => 
 $@"namespace {nameSpace} {{
     {string.Join("\n", code.Split('\n').Select(l => "    " + l))}

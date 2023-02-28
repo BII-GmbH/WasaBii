c8445cb47dede64f121d5f1f9ebf0e5beff2d1b2
Gollorum <david-schantz@gmx.de>
2023-02-28 10:44:54 +0100

    WithinUnitValue doc comment

--- WasaBii-unity-project/Packages/WasaBii/WasaBii-Units/Tests/UnitValueEqualityExtension.cs
+++ WasaBii-unity-project/Packages/WasaBii/WasaBii-Units/Tests/UnitValueEqualityExtension.cs
@@ -5,9 +5,19 @@ namespace BII.WasaBii.UnitSystem.Tests
     public static class UnitValueEqualityExtension
     {
 
+        /// <summary>
+        /// Like <see cref="EqualConstraint.Within"/>, but works for <see cref="IUnitValue{T}"/>. Configures
+        /// the <see cref="EqualConstraint"/> such that the equality is checked within a <paramref name="tolerance"/>.
+        /// </summary>
+        /// <example>
+        /// assert that duration == 1min ± 1sec:
+        /// Assert.That(duration.AsSeconds(), Is.EqualTo(1.0.Minute().AsSeconds()).Within(1.0));
+        /// ==
+        /// Assert.That(duration, Is.EqualTo(1.0.Minute()).WithinUnitValue(1.0.Seconds()));
+        /// </example>
         public static EqualConstraint WithinUnitValue<T>(this EqualConstraint constraint, T tolerance)
         where T : struct, IUnitValue<T> => 
-            constraint.Within(tolerance).Using<T>((expected, actual) => 
+            constraint.Using<T>((expected, actual) => 
                 expected.IsNearly(actual, tolerance) ? 0 : expected.SiValue.CompareTo(actual.SiValue));
 
     }

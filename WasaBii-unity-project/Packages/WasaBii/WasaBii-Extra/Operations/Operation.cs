e3fac38a1fa490ee27cc12f0ce426f479893d947
Cameron Reuschel <reuschel.cameron@live.de>
2023-04-14 04:06:10 +0200

    minor cleanup

--- WasaBii-unity-project/Packages/WasaBii/WasaBii-Extra/Operations/Operation.cs
+++ WasaBii-unity-project/Packages/WasaBii/WasaBii-Extra/Operations/Operation.cs
@@ -5,61 +5,9 @@ using BII.WasaBii.Core;
 
 namespace BII.WasaBii.Extra
 {
-    // TODOS:
-    // - custom error foo and Validate(Step)
+    // TODO: consider variants with Error, incorporating Result in the monad transformer stack
+    // TODO: ensure that exceptions are debuggable, especially when thrown on another thread
 
-    public interface OperationStepContext
-    {
-        Action<double> ReportProgressInStep { get; init; }
-        CancellationToken? CancellationToken { get; init; }
-    }
-
-    public record OperationContext(
-        Action<string> OnStepStarted,
-        Action<int> OnStepCompleted,
-        Action<int> OnNewStepCount,
-        Action<double> ReportProgressInStep, 
-        CancellationToken? CancellationToken
-    ) : OperationStepContext {
-        public OperationContext<T> WithStartValue<T>(T startValue) => new(
-            startValue,
-            OnStepStarted,
-            OnStepCompleted,
-            OnNewStepCount,
-            ReportProgressInStep,
-            CancellationToken
-        );
-
-        public OperationContext WithStepOffset(int stepsToAdd) =>
-            this with {OnStepCompleted = steps => OnStepCompleted(steps + stepsToAdd)};
-        
-        public OperationContext WithStepCountOffset(int stepsToAdd) =>
-            this with {OnNewStepCount = steps => OnNewStepCount(steps + stepsToAdd)};
-    }
-
-    public record OperationContext<T>(
-        T StartValue,
-        Action<string> OnStepStarted,
-        Action<int> OnStepCompleted,
-        Action<int> OnNewStepCount,
-        Action<double> ReportProgressInStep, 
-        CancellationToken? CancellationToken
-    ) : OperationContext(OnStepStarted, OnStepCompleted, OnNewStepCount, ReportProgressInStep, CancellationToken) {
-        public OperationContext WithoutStartValue() => new(
-            OnStepStarted,
-            OnStepCompleted,
-            OnNewStepCount,
-            ReportProgressInStep,
-            CancellationToken
-        );
-        
-        public new OperationContext<T> WithStepOffset(int stepsToAdd) =>
-            this with {OnStepCompleted = steps => OnStepCompleted(steps + stepsToAdd)};
-        
-        public new OperationContext<T> WithStepCountOffset(int stepsToAdd) =>
-            this with {OnNewStepCount = steps => OnNewStepCount(steps + stepsToAdd)};
-    }
-    
     public record Operation(int EstimatedStepCount, Func<OperationContext, Task> Run)
     {
         public static Operation Empty => new(0, _ => Task.CompletedTask);
---
+++ WasaBii-unity-project/Packages/WasaBii/WasaBii-Extra/Operations/OperationContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace BII.WasaBii.Extra
+{
+    public interface OperationStepContext
+    {
+        Action<double> ReportProgressInStep { get; init; }
+        CancellationToken? CancellationToken { get; init; }
+    }
+
+    public record OperationContext(
+        Action<string> OnStepStarted,
+        Action<int> OnStepCompleted,
+        Action<int> OnNewStepCount,
+        Action<double> ReportProgressInStep, 
+        CancellationToken? CancellationToken
+    ) : OperationStepContext {
+        public OperationContext<T> WithStartValue<T>(T startValue) => new(
+            startValue,
+            OnStepStarted,
+            OnStepCompleted,
+            OnNewStepCount,
+            ReportProgressInStep,
+            CancellationToken
+        );
+
+        public OperationContext WithStepOffset(int stepsToAdd) =>
+            this with {OnStepCompleted = steps => OnStepCompleted(steps + stepsToAdd)};
+        
+        public OperationContext WithStepCountOffset(int stepsToAdd) =>
+            this with {OnNewStepCount = steps => OnNewStepCount(steps + stepsToAdd)};
+    }
+
+    public record OperationContext<T>(
+        T StartValue,
+        Action<string> OnStepStarted,
+        Action<int> OnStepCompleted,
+        Action<int> OnNewStepCount,
+        Action<double> ReportProgressInStep, 
+        CancellationToken? CancellationToken
+    ) : OperationContext(OnStepStarted, OnStepCompleted, OnNewStepCount, ReportProgressInStep, CancellationToken) {
+        public OperationContext WithoutStartValue() => new(
+            OnStepStarted,
+            OnStepCompleted,
+            OnNewStepCount,
+            ReportProgressInStep,
+            CancellationToken
+        );
+        
+        public new OperationContext<T> WithStepOffset(int stepsToAdd) =>
+            this with {OnStepCompleted = steps => OnStepCompleted(steps + stepsToAdd)};
+        
+        public new OperationContext<T> WithStepCountOffset(int stepsToAdd) =>
+            this with {OnNewStepCount = steps => OnNewStepCount(steps + stepsToAdd)};
+    }
+}
